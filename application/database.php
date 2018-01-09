746cd62b4c010767231fff3ec22fb56d9c6ad2be
彭棕 <pengzong771@live.com>
2018-01-09 15:52:14 +0800

    Update database.php

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '39.104.68.145',
+    'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'mskp',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'Keepao2018',
+    'password'        => 'root',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
