83335edfe75ab5ab8a7feb3448a8c9ae8ee1202a
彭棕 <pengzong771@163.com>
2018-01-11 10:36:32 +0800

    排行榜创建
    排行榜创建

---
+++ application/index/model/Sport.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PengZong
+ * Date: 18/1/11
+ * Time: 上午10:31
+ */
+
+namespace app\index\model;
+
+
+class Sport extends Common{
+    public function getDataList(){
+
+    }
+}
